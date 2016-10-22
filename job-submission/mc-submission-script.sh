2c9b7d5232d6df1b071e7aa5879324e8821a005a
chris <chris@Starace>
2016-10-22 19:48:21 -0400

    Fix submission scrips on acorite and mc

--- job-submission/mc-submission-script.sh
+++ job-submission/mc-submission-script.sh
@@ -11,8 +11,8 @@ DATAPATH="/share/apps/MCNP/MCNP_DATA"
 EXE="/share/apps/mcnp/MCNP_CODE/MCNP6/bin/mcnp6.mpi"
 MPI="/share/apps/openmpi-1.10.2/bin/mpirun"
 LD_LIBRARY_PATH="/share/apps/gcc-5-3-0/lib64:$LD_LIBRARY_PATH"
-MCNP_INPUT='i="'$1.'inp" o="'$1'.out" srctp="'$1'.srctp" mctal="'$1'.mctal" runtpe="'$1'.runtpe" > '$2
-COMMAND="$MPI $EXE $MCNP"
+INPUT='i="'$1.'inp" o="'$1'.out" srctp="'$1'.srctp" mctal="'$1'.mctal" runtpe="'$1'.runtpe" > '$2
+COMMAND="$MPI $EXE $INPUT"
 
 echo $COMMAND
 
